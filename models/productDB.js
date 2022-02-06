8e44123b235f89ac3c15c60eddc29ab29cbd4560
hanwen-zhang6 <hanwen_zhang@apple.com>
2022-02-05 20:06:27 -0500

    added some boiler plates code

--- models/productDB.js
+++ models/productDB.js
@@ -1,2 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  address: { type: String, required: true },
+  creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+});
+
+module.exports = mongoose.model("Product", productSchema);
--- models/userDB.js
+++ models/userDB.js
@@ -1,2 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const uniqueValidator = require("mongoose-unique-validator");
+
+const userSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 6 },
+  product: [{ type: mongoose.Types.ObjectId, required: true, ref: "Product" }],
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("User", userSchema);
